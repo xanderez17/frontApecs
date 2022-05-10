4fd3aa7031f6c182f39a6c9695f158ff7b7bc34d
xanderez17 <kevxanderez17@gmail.com>
2022-05-10 09:40:42 -0500

    Listado ID en tabla Kevin - Sin interfaces

--- src/app/interfaces/menu.ts
+++
@@ -1,4 +0,0 @@
-export interface Menu{
-  nombre: string;
-  redirect: string;
-}
--- src/app/interfaces/usuario.ts
+++
@@ -1,6 +0,0 @@
-export interface Usuario{
-  usuario: string;
-  nombre: string;
-  apellido: string;
-  sexo: string;
-}
